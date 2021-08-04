37f08f25bcf6fb34b8331257e7aaeba078c51fc0
Jacek Czaja <jacek.czaja@intel.com>
2021-08-04 14:30:52 +0200

    - Added create custom action

--- corporate-assistant/src/ca.rs
+++ corporate-assistant/src/ca.rs
@@ -6,7 +6,17 @@ pub mod actions {
 
     impl CorporateAction for CreateCustomAction {
         fn Run(&self, tts: &mut tts::TTS) -> () {
-            todo!();
+            tts.speak("Please edit custom action script", true)
+                .expect("Problem with utterance");
+            // Get user script name and pass it to be executed
+            let mut script_name = std::env::current_exe().unwrap();
+            script_name.pop();
+            script_name.pop();
+            script_name.pop();
+            script_name.push("custom_action");
+            script_name.push("scripts");
+            script_name.push("custom_script.sh");
+            action_creator(script_name);
         }
     }
 
